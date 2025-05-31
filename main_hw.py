b2b270ee619b02946607d987f27d8dd234eac841
Aarush112-dev <aarushmenon2020@gmail.com>
2025-05-31 11:33:44 +0100

    Update main_hw.py

--- main_hw.py
+++ main_hw.py
@@ -16,40 +16,51 @@ data["Institution"].fillna("unknown",inplace=True)
 data.info()
 
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
+# from sklearn.compose import ColumnTransformer
+# from sklearn.preprocessing import OneHotEncoder
+# from sklearn.model_selection import train_test_split
 
 
-x = data.iloc[:,:3].values
-y=data["Country"].values
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-y = le.fit_transform(y)
+# x = data[["Degree","Profession","Name","Field"]]
+# y=data["Country"].values
+# from sklearn.preprocessing import LabelEncoder
+# le = LabelEncoder()
+# y = le.fit_transform(y)
+# ct = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),["Degree","Profession","Name","Field"])])
+# x = ct.fit_transform(x)
 
-X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.3,random_state=1)
+# X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.3,random_state=1)
 
 
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import metrics
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn import metrics
 
-model = DecisionTreeClassifier(max_depth=100,random_state=50,criterion="gini")
-model.fit(X_train,Y_train)
-predictions = model.predict(X_test)
-accuracy = metrics.accuracy_score(predictions,Y_test)
-print(accuracy)
+# model = DecisionTreeClassifier(max_depth=100,random_state=50,criterion="gini")
+# model.fit(X_train,Y_train)
+# predictions = model.predict(X_test)
+# accuracy = metrics.accuracy_score(predictions,Y_test)
+# print(accuracy)
 
-model = DecisionTreeClassifier(max_depth=100,random_state=50,criterion="entropy")
-model.fit(X_train,Y_train)
-predictions = model.predict(X_test)
-accuracy = metrics.accuracy_score(predictions,Y_test)
-print(accuracy)
+# model = DecisionTreeClassifier(max_depth=100,random_state=50,criterion="entropy")
+# model.fit(X_train,Y_train)
+# predictions = model.predict(X_test)
+# accuracy = metrics.accuracy_score(predictions,Y_test)
+# print(accuracy)
 
 #graphs
-data[data["Field"]=="Entrepreneur"]
+print(data["Profession"].unique())
+data2 = data[data["Profession"]=="Entrepreneur"]
+print(data2.shape)
+count = data2["Institution"].value_counts().reset_index()
+print(count)
 
 figure3 = go.Figure(
-    go.Scatter(x=x,y=data["Institution"],fill="tonexty",line_color="purple")
+    go.Scatter(x=data2["Degree"],y=data["Institution"],mode="markers")
 )
 figure3.update_layout(title="Institutions of entrepreneurs")
 figure3.write_html("int.html",auto_open=True)
+
+# Figure = go.Figure(
+#     go.Bar(x=data2[])
+
+# )
